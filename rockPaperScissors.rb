93749b88f5b674681df75c11c4adb21d12c38042
Lehola <sunlight0154@rocketmail.com>
2014-10-19 16:00:54 -0400

    Advanced user error handing
    Advanced user error handing, including allowing the user to quit before entering a choice, asking them if would like to reply and a random sentence that appears when asking the user if they want to replay.

--- rockPaperScissors.rb
+++ rockPaperScissors.rb
@@ -1,77 +1,100 @@
 #premade variables/arrays/code blocks
 gameList = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
-choice = "Rock"
+retrySentances = "That was a great game", "Sweet game", "Not bad of a game", "Awesome game"
+
+begin
+
+    choice = "Rock"
 
 puts <<EOP
-\nWelcome to the game of Rock Paper Lizard Spock!
-the game is simple you choose either Rock, Paper, Lizard, Spock and I choose one to
-If we chosoe the same thing its a tie, other than that... see below
+    \nWelcome to the game of Rock Paper Lizard Spock!
+    the game is simple you choose either Rock, Paper, Lizard, Spock and I choose one to
+    If we chosoe the same thing its a tie, other than that... see below
 
-    ~Scissors cut paper
-    ~Paper covers rock
-    ~Rock crushes lizard
+        ~Scissors cut paper
+        ~Paper covers rock
+        ~Rock crushes lizard
 
-    ~Lizard poisons Spock
-    ~Spock smashes scissors
-    ~Scissors decapitate lizard
-    ~Lizard eats paper
+        ~Lizard poisons Spock
+        ~Spock smashes scissors
+        ~Scissors decapitate lizard
+        ~Lizard eats paper
 
-    ~Paper disproves Spock
-    ~Spock vaporizes rock
-    ~Rock crushes scissors
+        ~Paper disproves Spock
+        ~Spock vaporizes rock
+        ~Rock crushes scissors
 
+    -Reminder, you can type Quit anytime to exit-
 EOP
 
-begin 
+    begin 
 
-    if choice != "Rock" && choice != "Paper" &&  choice != "Scissors" && choice != "Lizard" &&  choice != "Spock"
-        puts "\n~Invalid choice, please try again "
-             
-    end
+        if choice != "Rock" && choice != "Paper" &&  choice != "Scissors" && choice != "Lizard" &&  choice != "Spock"
+            puts "\n~Invalid choice, please try again "
+                 
+        end
 
-    print "Please enter your choice: "
-    choice = gets.chomp.capitalize
+        print "Please enter your choice: "
+        choice = gets.chomp.capitalize
 
-end while choice != "Rock" && choice != "Paper" &&  choice != "Scissors" && choice != "Lizard" &&  choice != "Spock" #user error handling, in case they don't enter a choice
+    end while choice != "Rock" && choice != "Paper" &&  choice != "Scissors" && choice != "Lizard" &&  choice != "Spock" && choice != "Quit"  #user error handling, in case they don't enter a choice
 
-ourChoice = gameList[rand(gameList.length)]
+    ourChoice = gameList[rand(gameList.length)]
 
-result = 
-if choice == "Scissors" &&  ourChoice == "Paper"
-    "You win!"
+    unless choice == "Quit"
 
-	elsif choice == "Paper" &&  ourChoice == "Rock"
-        "You win!"
+        result = 
+        if choice == ourChoice
+             "its a tie!"
 
-    elsif choice == "Rock" &&  ourChoice == "Lizard"
-        "You win!"
+        	elsif choice == "Paper" &&  ourChoice == "Rock"
+                "You win!"
 
-    elsif choice == "Lizard" &&  ourChoice == "Spock"
-        "You win!"
+            elsif choice == "Rock" &&  ourChoice == "Lizard"
+                "You win!"
 
-    elsif choice == "Spock" &&  ourChoice == "Scissors"
-        "You win!"
+            elsif choice == "Lizard" &&  ourChoice == "Spock"
+                "You win!"
 
-    elsif choice == "Scissors" &&  ourChoice == "Lizard"
-        "You win!"
+            elsif choice == "Spock" &&  ourChoice == "Scissors"
+                "You win!"
 
-    elsif choice == "Lizard" &&  ourChoice == "Paper"
-        "You win!"
+            elsif choice == "Scissors" &&  ourChoice == "Lizard"
+                "You win!"
 
-    elsif choice == "Paper" &&  ourChoice == "Spock"
-        "You win!"
+            elsif choice == "Lizard" &&  ourChoice == "Paper"
+                "You win!"
 
-    elsif choice == "Spock" &&  ourChoice == "Rock"
-         "You win!"
+            elsif choice == "Paper" &&  ourChoice == "Spock"
+                "You win!"
 
-    elsif choice == "Rock" &&  ourChoice == "Scissors"
-         "You win!"
+            elsif choice == "Spock" &&  ourChoice == "Rock"
+                "You win!"
 
-    elsif  choice == ourChoice
-         "its a tie!"
+            elsif choice == "Rock" &&  ourChoice == "Scissors"
+                "You win!"
 
-        else    
-             "You lose!"
-end
+            elsif  choice == "Scissors" &&  ourChoice == "Paper"
+                "You win!"
 
-puts "\nYou chose #{choice} and we chose #{ourChoice} which means #{result}!"
+                else    
+                     "You lose!"
+        end#End of if choice statement
+
+            puts "\nYou chose #{choice} and we chose #{ourChoice} which means #{result}!\n"
+               generatedSen = retrySentances[rand(retrySentances.length)]
+     
+        begin
+            print generatedSen + ", play again(Yes/No)? :"
+            choice = gets.chomp.capitalize
+            
+             if choice != "Yes" && choice != "No"
+               puts "\n~Invalid choice, please try again \n"
+                 
+            end
+
+        end while choice != "Yes" && choice != "No"
+
+    end#End of unless quit statment, skips everything if user quits
+
+end while choice!= "No" && choice != "Quit"
