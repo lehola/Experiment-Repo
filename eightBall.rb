1659168e16a5da9d834ff9a6720d3e9d73af4d3b
Lehola <sunlight0154@rocketmail.com>
2014-10-31 14:04:25 -0400

    Starting object oriented
    Working on making the program more object oriented. Still needs work

--- eightBall.rb
+++ eightBall.rb
@@ -1,7 +1,15 @@
-answersList = File.readlines("answersBase.txt")
+class EightBall
+ def main
+   
+		answersList = File.readlines("answersBase.txt")
 
-puts "Weclome, here you can use the magical eight-ball to answer all your questions."
-print "please enter your question: "
-gets
+		puts "----------------------------------------"
+		puts "\nWeclome, here you can use the magical eight-ball to answer all your questions."
+		print "please enter your question: "
+		gets
 
-puts "\n~#{answersList.sample}"
+		puts "\n~#{answersList.sample}"
+  	puts "----------------------------------------"
+
+ end
+end
