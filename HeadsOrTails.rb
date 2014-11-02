27fbf2f67ba0f1af0b0c1e7643fdb564f2fa1c3a
Lehola <sunlight0154@rocketmail.com>
2014-11-02 17:16:07 -0500

    Create HeadsOrTails.rb

---
+++ HeadsOrTails.rb
@@ -0,0 +1,16 @@
+class HeadsOrTails
+ def main	
+
+ 	puts "----------------------------------------"
+
+	randomNumber = rand(2)
+
+	if (randomNumber == 0)
+		puts "Heads"
+
+	elsif
+		puts "Tails"
+
+	end #End of if statement
+ end
+end
