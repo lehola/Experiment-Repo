14eb551078e4e05ee0e57f150c09d916328fb02d
Lehola <sunlight0154@rocketmail.com>
2014-11-04 18:31:45 -0500

    Added String Reverser sort of game

--- Main.rb
+++ Main.rb
@@ -2,12 +2,14 @@ require_relative "EightBall.rb"
 require_relative "RockPaperScissors.rb"
 require_relative "HeadsOrTails.rb"
 require_relative "LoveCalulator.rb"
+require_relative "StringReverse.rb"
 
 retrySentances = "That was a great game", "Sweet game", "Not bad of a game", "Awesome game", "Nice", "Pretty good game" 
 GameEightBall = EightBall.new
 GameRockPaperScissors = RockPaperScissors.new
 GameHeadsOrTails = HeadsOrTails.new
 GameLoveCalulator = LoveCalulator.new
+GameStringReverse = StringReverse.new
 
 begin
 	puts <<EOF
@@ -19,12 +21,14 @@ begin
 	~Rock paper scissors spock(r)
 	~Heads or tails(h)
 	~Love Calulator(l)
+	~String Reverser(s)
 EOF
 
 	print "\nplease, ener your choice: "
 	userChoice = gets[0].downcase
 		
 	if userChoice != 'q'
+
 		begin
 
 		case 
@@ -40,10 +44,14 @@ EOF
 			when userChoice == 'l'
 				GameLoveCalulator.main
 
+			when userChoice == 's'
+				GameStringReverse.main
+
 			when userChoice != 'q'
 				puts "*********$$$$* invalid, please enter from the list ******$$$$$****"	
 
 		end #End of case statement
+
 
 		print "Would you like to play again (y)es, (q)uit?:"
 		redoChoice = gets[0].downcase
