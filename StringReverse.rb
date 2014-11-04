7c59e1a08a67b796de1a29431f802df2331ab58c
Lehola <sunlight0154@rocketmail.com>
2014-11-04 18:58:32 -0500

    Forogt to add a :

--- StringReverse.rb
+++ StringReverse.rb
@@ -2,7 +2,7 @@ class StringReverse
  def main
 
  	puts "----------------------------------------"
-	print "Please, enter your String "
+	print "Please, enter your String: "
 	word = gets.chomp.reverse.capitalize
 
 	puts "\nand your string in reverse is: #{word}" 
