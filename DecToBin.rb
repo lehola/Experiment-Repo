fa634f17cc111f1fe4cc03fedce2136d418872da
Lehola <sunlight0154@rocketmail.com>
2014-11-06 13:57:04 -0500

    Create DecToBin.rb

---
+++ DecToBin.rb
@@ -0,0 +1,11 @@
+class DecToBin
+ def main 
+ 	
+ 	puts "\nWelcome, here we can convert any decmail mumber(I.E base 10) to its binary form!"
+ 	print "Enter your decimal number: "
+ 	decNum = gets.to_i
+
+ 	puts "\n~The decimal #{decNum} in Binary is: #{decNum.to_s(2)}"
+
+ end
+end
